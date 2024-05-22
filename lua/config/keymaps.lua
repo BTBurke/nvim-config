05714857a76b8b4b00bff9404a5ca13a839aec79
Bryan Burke <btburke@github>
2024-05-22 15:45:54 +0500

    add which-key, surround

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -19,50 +19,68 @@ wk.register({
     g = { "<cmd>Telescope live_grep", "Find in files"},
     n = { "New File" },
   },
-
+  l = {
+    name = "language",
+    t = { "<cmd>GoTest -v<cr>", "Go test (current pkg)"},
+    f = { "<cmd>GoTest -f<cr>", "Go test (current file)"},
+    n = { "<cmd>GoTest -n<cr>", "Go test (nearest testfunc)"},
+    s = { "<cmd>GoTestSum<cr>", "Go test summary"},
+    o = { "<cmd>GoAlt!<cr>", "Open matching impl/test file"},
+    d = { "<cmd>GoDoc<cr>", "Show Go doc"},
+  },
+  t = {
+    name = "terminal",
+    t = { "<cmd>ToggleTerm<cr>", "Toggle terminal"},
+    b = { "<cmd>lua _gitbug_toggle()<cr>", "Open git bug in termui"},
+  },
+  w = {
+    name = "window",
+    h = {"<cmd>wincmd h<cr>", "Move to left"},
+    j = {"<cmd>wincmd j<cr>", "Move to below"},
+    k = {"<cmd>wincmd k<cr>", "Move to above"},
+    l = {"<cmd>wincmd l<cr>", "Move to right"},
+  }
 }, { prefix = "<leader>" })
-
--- Fast saving with <leader> and s
-
-
-
--- vim.keymap.set("n", "<leader>s", ":w<CR>", {})
--- Move around splits
--- vim.keymap.set("n", "<leader>wh", "<C-w>h", {})
--- vim.keymap.set("n", "<leader>wj", "<C-w>j", {})
--- vim.keymap.set("n", "<leader>wk", "<C-w>k", {})
--- vim.keymap.set("n", "<leader>wl", "<C-w>l", {})
-
--- Reload configuration without restart nvim
--- vim.keymap.set("n", "<leader>r", ":so %<CR>", {})
-
--- Telescope
--- <leader> is a space now
---local builtin = require("telescope.builtin")
---vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
---vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
---vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
---vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
 
 -- NvimTree
 -- vim.keymap.set("n", "<leader>n", ":NvimTreeToggle<CR>", {}) -- open/close
 -- vim.keymap.set("n", "<leader>nr", ":NvimTreeRefresh<CR>", {}) -- refresh
 -- vim.keymap.set("n", "<leader>nf", ":NvimTreeFindFile<CR>", {}) -- search file
 
--- Terminal
--- vim.keymap.set("n", "<leader>tt", ":ToggleTerm<CR>", {})
--- vim.keymap.set("n", "<leader>tx", ":NeotermExit<CR>", {})
 
--- function _G.set_terminal_keymaps()
---  local opts = {buffer = 0}
---  vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
---  vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opts)
---  vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
---  vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
---  vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
---  vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
---  vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], opts)
--- end
+local Terminal  = require('toggleterm.terminal').Terminal
+-- run git bug in a floating terminal window. Overrides editor to use nano because
+-- nvim in nvim window is a pain
+local gitbug = Terminal:new({
+  cmd = "EDITOR=/usr/bin/nano git bug termui",
+  dir = "git_dir",
+  hidden = true,
+  display_name = "Issues",
+  direction = "float",
+  float_opts = {
+    border = "double"
+  },
+  on_open = function(term)
+    vim.cmd("startinsert!")
+    vim.api.nvim_buf_set_keymap(term.bufnr, "n", "q", "<cmd>close<CR>", {noremap = true, silent = true})
+  end,
+})
+
+function _gitbug_toggle()
+  gitbug:toggle()
+end
+
+function _G.set_terminal_keymaps()
+  local opts = {buffer = 0}
+  vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
+  vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opts)
+  vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
+  vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
+  vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
+  vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
+  vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], opts)
+  vim.keymap.set('t', '<C-q>', [[<Cmd>q<cr>]], opts)
+end
 
 -- if you only want these mappings for toggle term use term://*toggleterm#* instead
--- vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
+vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -8,8 +8,6 @@
 --
 -- File: plugins/lsp.lua
 -- Description: LSP setup and config
--- Author: Kien Nguyen-Tuan <kiennt2609@gmail.com>
--- Enable some language servers with the additional completion capabilities offered by nvim-cmp
 return {{
     -- Mason
     "williamboman/mason.nvim",
@@ -71,9 +69,9 @@ return {{
             dockerls = {},
             bashls = {},
             gopls = {},
-            ruff_lsp = {},
             vimls = {},
-            yamlls = {}
+            yamlls = {},
+            tsserver = {}
         },
         -- you can do any additional lsp server setup here
         -- return true if you don"t want this server to be setup with lspconfig
@@ -182,8 +180,8 @@ return {{
             })
         end
     },
-                    {"saadparwaiz1/cmp_luasnip", "hrsh7th/cmp-nvim-lua", "hrsh7th/cmp-nvim-lsp", "hrsh7th/cmp-buffer",
-                     "hrsh7th/cmp-path"}}, -- cmp sources plugins
+      {"saadparwaiz1/cmp_luasnip", "hrsh7th/cmp-nvim-lua", "hrsh7th/cmp-nvim-lsp", "hrsh7th/cmp-buffer", "hrsh7th/cmp-path"}
+    }, -- cmp sources plugins
     opts = function()
         local cmp = require "cmp"
 
@@ -194,11 +192,15 @@ return {{
 
         local options = {
             completion = {
-                completeopt = "menu,menuone"
+                autocomplete = false,
+                completeopt = "menu,menuone",
+                winhighlight = "Normal:CmpPmenu,CursorLine:CmpSel,Search:PmenuSel",
+                border = border "CmpDocBorder",
             },
 
             window = {
                 completion = {
+                    autocomplete = false,
                     winhighlight = "Normal:CmpPmenu,CursorLine:CmpSel,Search:PmenuSel",
                     scrollbar = false
                 },
@@ -207,66 +209,32 @@ return {{
                     winhighlight = "Normal:CmpDoc"
                 }
             },
-
             snippet = {
                 expand = function(args)
                     require("luasnip").lsp_expand(args.body)
                 end
             },
-
             mapping = {
-                ["<C-p>"] = cmp.mapping.select_prev_item(),
-                ["<C-n>"] = cmp.mapping.select_next_item(),
-                ["<C-d>"] = cmp.mapping.scroll_docs(-4),
-                ["<C-f>"] = cmp.mapping.scroll_docs(4),
+                -- completion is only activated manually with Ctrl-Space
+                -- up/down to page through options, tab/enter to select
+                ["<Down>"] = cmp.mapping.select_next_item(),
+                ["<Up>"] = cmp.mapping.select_prev_item(),
                 ["<C-Space>"] = cmp.mapping.complete(),
-                ["<C-e>"] = cmp.mapping.close(),
+                ["<C-esc"] = cmp.mapping.close(),
+                ["<Tab>"] = cmp.mapping.confirm {
+                    behavior = cmp.ConfirmBehavior.Insert,
+                    select = true
+                },
                 ["<CR>"] = cmp.mapping.confirm {
                     behavior = cmp.ConfirmBehavior.Insert,
                     select = true
                 },
-                ["<Tab>"] = cmp.mapping(function(fallback)
-                    if cmp.visible() then
-                        cmp.select_next_item()
-                    elseif require("luasnip").expand_or_jumpable() then
-                        vim.fn.feedkeys(
-                            vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true), "")
-                    else
-                        fallback()
-                    end
-                end, {"i", "s"}),
-                ["<S-Tab>"] = cmp.mapping(function(fallback)
-                    if cmp.visible() then
-                        cmp.select_prev_item()
-                    elseif require("luasnip").jumpable(-1) then
-                        vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-jump-prev", true, true, true), "")
-                    else
-                        fallback()
-                    end
-                end, {"i", "s"})
             },
             sources = {{
                 name = "nvim_lsp"
-            }, {
-                name = "luasnip"
-            }, {
-                name = "buffer",
-                option = {
-                    -- Avoid accidentally running on big files
-                    get_bufnrs = function()
-                        local buf = vim.api.nvim_get_current_buf()
-                        local byte_size = vim.api.nvim_buf_get_offset(buf, vim.api.nvim_buf_line_count(buf))
-                        if byte_size > 1024 * 1024 then -- 1 Megabyte max
-                            return {}
-                        end
-                        return {buf}
-                    end
-                }
-            }, {
-                name = "nvim_lua"
-            }, {
-                name = "path"
-            }}
+            },{
+                name = "buffer"
+            }},
         }
 
         return options
